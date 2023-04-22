14a41ec000ff2dc437d7e6a2f7d59547ff216ead
Simon Kang <xeungjin@nate.com>
2023-04-22 16:35:32 +0900

    login test

--- web-study-09-02/src/main/java/com/saeyan/controller/LoginServlet.java
+++ web-study-09-02/src/main/java/com/saeyan/controller/LoginServlet.java
@@ -8,6 +8,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.saeyan.dao.MemberDAO;
+import com.saeyan.dto.MemberVO;
 
 /**
  * Servlet implementation class LoginServlet
@@ -17,7 +21,8 @@ public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("member/login.jsp");
@@ -25,11 +30,33 @@ public class LoginServlet extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		String url = "member/login.jsp";
 
+		String userid = request.getParameter("userid");
+		String pwd = request.getParameter("pwd");
+
+		MemberDAO mDao = MemberDAO.getInstance();
+		int result = mDao.userCheck(userid, pwd);
+
+		System.out.println(result);
+		
+		if (result == 1) {
+			MemberVO mVo = mDao.getMember(userid);
+			HttpSession session = request.getSession();
+			session.setAttribute("loginUser", mVo);
+			request.setAttribute("message", "회원가입에 성공했습니다.");
+			url = "main.jsp";
+		} else if (result == 0) {
+			request.setAttribute("message", "비밀번호가 맞지 않습니다.");
+		} else if (result == -1) {
+			request.setAttribute("message", "존재하지 않는 회원입니다.");
+		}
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+		dispatcher.forward(request, response);
+	}
 }
